cbfdefdd9511d0adea0e65aadc06fadcf7a31c29
Posccis <lkzq33@gmail.com>
2023-12-28 23:40:29 -0300

    update(main)=Criei a controller de Colaborador e o endpoint para criar um colaborador

---
+++ Workshop.API/Controllers/ColaboradorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WorkshopMng.Domain.Domains;
+using WorkshopMng.Persistence.Context;
+
+namespace Workshop.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ColaboradorController : ControllerBase
+    {
+        private readonly WorkshopContext _context;
+
+        public ColaboradorController(WorkshopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarColaborador(Colaborador colaborador) 
+        { 
+            await _context.Colaboradores.AddAsync(colaborador);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetColaborador), new { id = colaborador.Id }, colaborador);
+        }
+
+        [HttpGet("{id}", Name = "GetColaborador")]
+        public async Task<IActionResult> GetColaborador(int id)
+        {
+            var colaborador = await _context.Colaboradores.FindAsync(id);
+
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(colaborador);
+        }
+    }
+}
--- Workshop.API/Program.cs
+++ Workshop.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using WorkshopMng.Persistence;
+using WorkshopMng.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WorkshopContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 
 var app = builder.Build();
 
@@ -16,10 +27,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<WorkshopContext>();
+    context.Database.Migrate();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Ocorreu um erro durante a migration.");
+}
 
 app.Run();
