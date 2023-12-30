7c7afc751e5b70522fca0785e9c2e952da150141
Posccis <lkzq33@gmail.com>
2023-12-30 00:03:39 -0300

    new(main)= Criei a controller de Workshop responsável pelos métodos de workshop

---
+++ Workshop.API/Controllers/WorkshopController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkshopMng.Domain.Domains;
+using WorkshopMng.Persistence.Context;
+
+namespace WorkshopMng.API.Controllers
+{
+    [ApiController]
+    [Route("api/workshops")]
+    public class WorkshopController : ControllerBase
+    {
+        private readonly WorkshopContext _context;
+
+        public WorkshopController(WorkshopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarWorkshop(WorkshopMng.Domain.Domains.Workshop workshop) 
+        { 
+            await _context.Workshops.AddAsync(workshop);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetWorkshop), new { id = workshop.Id }, workshop);
+        }
+
+        [HttpGet("{id}", Name = "GetWorkshop")]
+        public async Task<IActionResult> GetWorkshop(int id)
+        {
+            var workshop = await _context.Workshops.FindAsync(id);
+
+            if (workshop == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(workshop);
+        }
+    }
+}
