1aea0c864ec5e975e44e2f8f39f78f78f7302903
Posccis <lkzq33@gmail.com>
2024-01-02 10:49:24 -0300

    update(main)= conclusão do frontend

--- workshop-app/src/app/app.component.ts
+++ workshop-app/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import * as $ from 'jquery'; 
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'workshop-app';
+  title = 'Gerenciamento de Workshop';
 }
--- workshop-app/src/app/app.routes.ts
+++ workshop-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {AtasPageComponent} from './atas-page/atas-page.component'
 import { DashboardsPageComponent } from './dashboards-page/dashboards-page.component';
 export const routes: Routes = [
+    {path: '', component: AtasPageComponent},
     {path: 'atas', component: AtasPageComponent},
     {path: 'dashboards', component: DashboardsPageComponent}
 ];
