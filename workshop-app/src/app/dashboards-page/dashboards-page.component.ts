f8e0616c82b72ef56aaa260d484cb3eb4d1b2bd4
Posccis <lkzq33@gmail.com>
2024-01-02 10:48:01 -0300

    update(main)= Conclusão da página de dashboard

--- workshop-app/src/app/dashboards-page/dashboards-page.component.ts
+++ workshop-app/src/app/dashboards-page/dashboards-page.component.ts
@@ -1,16 +1,59 @@
 import { Component } from '@angular/core';
 import { BarplotComponent } from '../barplot/barplot.component';
+import { DashboardsPageService } from './dashboards-page.service';
+import { PizzaplotComponent } from "../pizzaplot/pizzaplot.component";
 
 
 @Component({
-  selector: 'app-dashboards-page',
-  standalone: true,
-  imports: [
-    BarplotComponent
-  ],
-  templateUrl: './dashboards-page.component.html',
-  styleUrl: './dashboards-page.component.css'
+    selector: 'app-dashboards-page',
+    standalone: true,
+    templateUrl: './dashboards-page.component.html',
+    styleUrl: './dashboards-page.component.css',
+    imports: [
+        BarplotComponent,
+        PizzaplotComponent
+    ]
 })
 export class DashboardsPageComponent {
 
+  quantidadeWorksshops: any[] = [];
+  colaboradores: string[] = [];
+
+  workshops: string[] = [];
+  quantidadeColaboradores: any[] = [];
+  constructor(private service: DashboardsPageService) {
+    
+  }
+
+  ngOnInit(){
+    this.obterDadosGraficoBarra();
+    this.obterDadosGraficoPizza();
+
+  }
+
+  obterDadosGraficoBarra(){
+    this.service.getQuantidadeWorkshopPorColaborador().subscribe(
+      (resultado) => {
+        this.colaboradores = Object.keys(resultado);
+        this.quantidadeWorksshops = [{data: Object.values(resultado), label: 'Quantidade de Workshops'
+      }];
+    },
+      (error) =>{
+        console.log(error);
+      }
+    );
+  }
+
+  obterDadosGraficoPizza(){
+    this.service.getQuantidadeColaboradorPorWorkshop().subscribe(
+      (resultado) => {
+        this.workshops = Object.keys(resultado);
+        this.quantidadeColaboradores = [{data: Object.values(resultado), label: 'Quantidade de Colaboradores'
+      }];
+      },
+      (error) =>{
+        console.log(error);
+      }
+    )
+  }
 }
--- workshop-app/src/app/dashboards-page/dashboards-page.service.ts
+++ workshop-app/src/app/dashboards-page/dashboards-page.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardsPageService {
+  private baseUrl = 'https://localhost:59088/api/';
 
   constructor(private http: HttpClient) {}
+
+  getQuantidadeWorkshopPorColaborador(): Observable<any>{
+    return this.http.get<any>(this.baseUrl + 'atas/colaboradores/workshops/');
+
+  }
+  getQuantidadeColaboradorPorWorkshop(): Observable<any>{
+    return this.http.get<any>(this.baseUrl + 'atas/workshops/colaboradores/');
+
+  }
 }
