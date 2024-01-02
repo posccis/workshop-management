ca9b9644b4afdddf87b6a87b4e4845f6424f8197
Posccis <lkzq33@gmail.com>
2024-01-02 10:47:03 -0300

    update(main)=Conclusão da páginas de atas

--- workshop-app/src/app/atas-page/atas-page.component.ts
+++ workshop-app/src/app/atas-page/atas-page.component.ts
@@ -1,13 +1,70 @@
 import { Component } from '@angular/core';
+import {NgFor, NgIf} from "@angular/common";
+import $ from 'jquery';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { AtasPageService } from './atas-page.service';
+import { Ata} from '../models/Ata';
+import { FiltroAtaDTO } from '../models/FiltroAtaDTO';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-atas-page',
     standalone: true,
     templateUrl: './atas-page.component.html',
     styleUrl: './atas-page.component.css',
-    imports: [SidebarComponent]
+    imports: [SidebarComponent, NgFor, NgIf, FormsModule]
 })
 export class AtasPageComponent {
 
+
+    idItemClicado: number | null = null;
+    numeroDeAtas: number | null = null;
+    atasList : Ata[] = [];
+    constructor(private service: AtasPageService) {
+        
+    }
+
+    ngOnInit(){
+        this.service.getAtas().subscribe(resultado => {
+            this.atasList = resultado;
+            this.numeroDeAtas = this.atasList.length;
+
+        });
+    }
+
+    filtraAtas(valor: NgForm){
+        const filtro: FiltroAtaDTO = {
+            NomeColaborador: valor.value.colaboradorNome,
+            NomeWorkshop: valor.value.workshopNome,
+            DataRealizacao: valor.value.dataSelecionada
+        };
+        this.service.getAtasFiltradas(filtro).subscribe((resultado) => {
+            console.log(resultado);
+            this.atasList = resultado;
+            this.numeroDeAtas = this.atasList.length;
+        },(error) => {
+            console.log(error.message);
+        });
+    }
+
+    toggleMostrarColaboradores(id: number) {
+        this.idItemClicado = this.idItemClicado === id ? null : id;
+      }
+    
+      isMostrarColaboradores(id: number) {
+        return this.idItemClicado === id;
+      }
+
+    retornaDia(data: string){
+        const dia = new Date(data).getDate();
+        return dia;
+    }
+    retornaMes(data: string){
+        const siglasDosMeses: string[] = [
+            'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+          ];
+        const mes = siglasDosMeses[new Date(data).getMonth()];
+
+        return mes.toUpperCase();
+    }
 }
--- workshop-app/src/app/atas-page/atas-page.service.ts
+++ workshop-app/src/app/atas-page/atas-page.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ata } from '../models/Ata';
+import { FiltroAtaDTO } from '../models/FiltroAtaDTO';
 
 
 @Injectable({
@@ -7,5 +10,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AtasPageService {
 
+  private baseUrl = 'https://localhost:59088/api/';
+
   constructor(private http: HttpClient) {}
+
+  getAtas(): Observable<Ata[]>{
+    return this.http.get<Ata[]>(this.baseUrl + 'atas/');
+  }
+
+  getAtasFiltradas(filtro: FiltroAtaDTO): Observable<Ata[]>{
+    const params = new URLSearchParams(filtro as any).toString();
+    console.log(this.baseUrl + `atas/filtro?${params}`);
+    return this.http.get<Ata[]>(this.baseUrl + `atas/filtro?${params}`);
+  }
 }
