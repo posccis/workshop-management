be47ed40622628108ceaf21a98f28a27f0ea872a
Posccis <lkzq33@gmail.com>
2024-01-02 10:46:44 -0300

    update(main)=Conclusão do componente de sidebar

--- workshop-app/src/app/sidebar/sidebar.component.ts
+++ workshop-app/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgClass, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  @Input() paginaAtual: string='';
 
+  validaPaginaAtual(pagina: string): any{
+    return pagina == this.paginaAtual ? 'active' : '';
+  }
 }
